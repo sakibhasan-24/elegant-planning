2793f1719ecae26467fa39eb857bda8995d93b61
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-23 00:53:36 +0600

    design logIn and registration

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Pages/Home";
 import Login from "../components/Pages/Login";
 import Register from "../components/Pages/Register";
+import About from "../components/Pages/About";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     element: <Home />,
     children: [
       { path: "/login", element: <Login /> },
+      { path: "/about", element: <About /> },
       { path: "/register", element: <Register /> },
     ],
   },
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,3 +1,51 @@
+import { FaGoogle } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="w-full mx-0 md:max-w-2xl lg:max-w-2xl md:mx-auto">
+      <h1 className="text-center font-semibold text-gray-500 text-2xl my-6">
+        Please Login
+      </h1>
+      <form className="shadow-2xl my-6 rounded-lg">
+        <div className="flex flex-col gap-6 items-center justify-center ">
+          <input
+            className="w-full md:w-3/4 px-6 py-3 border-2 border-green-300 rounded-lg focus:border-blue-950"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            required
+          />
+          <input
+            className="w-full md:w-3/4 px-6 py-3 border-2 border-green-300 rounded-lg focus:border-blue-950"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <input
+          type="submit"
+          value="Login"
+          className="px-8 py-2 border-0 rounded-lg font-bold text-white bg-green-600 text-center mx-16 lg:mx-64 md:mx-64 my-6"
+        />
+
+        <button
+          className="flex items-center justify-center gap-2 bg-blue-700 text-white px-6 py-2 rounded-md mx-auto my-4"
+          type="button"
+        >
+          <FaGoogle className=" font-bold text-xl" />
+          Sign In With Google
+        </button>
+        <p className="text-center font-bold">
+          didn't register?
+          <Link className="font-xl text-blue-800" to="/register">
+            Register
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
